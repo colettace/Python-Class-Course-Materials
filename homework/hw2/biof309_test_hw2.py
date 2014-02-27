b829b03b06e8cd126ada5dd749a05d6f720e9846
Chris Coletta <christopher.coletta@nih.gov>
2014-02-26 22:58:22 -0500

    test for SolicitInteger is done

--- homework/hw2/biof309_test_hw2.py
+++ homework/hw2/biof309_test_hw2.py
@@ -6,34 +6,92 @@ You can run this program on the provided byte-compiled solution to see that it w
 """
 import unittest
 import importlib
+import sys
 
 
 
+#=========================================================================
+class TestModule( object ):
+	student_module = None
+
+	def __init__( self ):
+		import os.path
+		modulename, _ = os.path.splitext( sys.argv[1] )
+		TestModule.student_module = importlib.import_module( modulename )
+
+	@staticmethod
+	def getMod():
+		return TestModule.student_module
+
+#=========================================================================
 class TestHW2( unittest.TestCase ):
 	"""All these test have to pass to get an A"""
 
-	def setUp( self ):
-		import sys
-		modulename = sys.argv[1]
-		# FIXME: strip .py out basename
-		self.student_module = importlib.import_module( modulename )
+	#============================================
+	def callSolicitInteger( self, inputs, expected_message ):
+		"""helper function"""
+		stu_mod = TestModule.getMod()
 
-	def test_SolicitInteger( self ):
-		"""signature: SolicitInteger(lobound=None, hibound=None, default_return=None)"""
+		for (lobound, hibound, default), _input, _output in inputs:
+			stu_mod.raw_input = lambda: _input
+			retval = None
+			retval = stu_mod.SolicitInteger( lobound, hibound, default )
+			try:
+				self.assertEqual( retval, _output )
+			except AssertionError:
+				errmsg = 'Your SolicitInteger returned a "{}" instead of a "{}" '.format(
+						retval, _output)
+				errmsg += "when calling with lobound={}, hibound={}, default_return={} ".format(
+						lobound, hibound, default )
+				errmsg += "and user input={}".format( _input )
+				self.fail( errmsg )
+			if expected_message:
+				try:
+					output = sys.stdout.getvalue().splitlines()[-1]
+					self.assertIn( expected_message, output.lower() )
+				except AssertionError:
+					errmsg = 'Your SolicitInteger should have printed something along the lines of ' +\
+							'"{}" instead of a "{}" '.format( expected_message, output)
+					errmsg += "when calling with lobound={}, hibound={}, default_return={} ".format(
+							lobound, hibound, default )
+					errmsg += "and user input={}".format( _input )
+					self.fail( errmsg )
+				except IndexError:
+					errmsg = 'Your SolicitInteger should have printed something along the lines of ' +\
+							'"{}" instead printed nothing '.format( expected_message )
+					errmsg += "when calling with lobound={}, hibound={}, default_return={} ".format(
+							lobound, hibound, default )
+					errmsg += "and user input={}".format( _input )
+					self.fail( errmsg )
 
-		assert( hasattr(sys.stdout, "getvalue") , "need to run in buffered mode" )
+		stu_mod.raw_input = raw_input
 
+	#============================================
+	def test_SolicitInteger_Garbage( self ):
+		"""Does your SolicitInteger function correctly handles garbage input?"""
 
 		invalid_garbage = (
 				( ( None, None, None ), "l jdf", None ),
 				( ( None, None, None ), "",      None ),
 				( ( None, None, None ), "19fgd", None ))
 
+		self.callSolicitInteger( invalid_garbage, 'invalid input' )
+
+	#============================================
+	def test_SolicitInteger_TooLow( self ):
+		"""Does your SolicitInteger function correctly handle user input that's too low?"""
+
 		invalid_toolow = (
 				( ( -10, None, None ), "-11",    None ), # standard case
 				( (   0, None, None ), "-1",     None ), # zero lbound case
-				( ( -10, None, None ), "0",      None ), # zero input case
+				( (  10, None, None ), "0",      None ), # zero input case
 				( ( -10, None,  -11 ), "",       None )) # default less than lbound case
+
+		self.callSolicitInteger( invalid_toolow, 'too lo' )
+
+	#============================================
+	def test_SolicitInteger_TooHi( self ):
+		"""Does your SolicitInteger function correctly handle user input that's too high?"""
 
 		invalid_toohigh = (
 				( ( None,  10, None ), "11",     None ), # standard case
@@ -41,12 +99,24 @@ class TestHW2( unittest.TestCase ):
 				( ( None, -10, None ), "0",      None ), # zero input case
 				( ( None,  10,   11 ), "",       None )) # default greater than ubound case
 
+		self.callSolicitInteger( invalid_toohigh, 'too hi' )
+
+	#============================================
+	def test_SolicitInteger_Default( self ):
+		"""Does your SolicitInteger function correctly handle defaults?"""
+
 		valid_default = (
 				( ( None, None, -10 ), "",      -10   ), # standard negative case
 				( ( None, None,  10 ), "",       10   ), # standard positive case
 				( ( None, None,   0 ), "",        0   ), # zero case
 				( ( -10, None,  -10 ), "",      -10   ), # default same as lbound case
 				( ( None, 10,    10 ), "",       10   )) # default same as ubound case
+
+		self.callSolicitInteger( valid_default, None )
+
+	#============================================
+	def test_SolicitInteger_Valid( self ):
+		"""Does your SolicitInteger function correctly handle user input that's too low?"""
 
 		valid = (
 				( ( None, None, None ), "-10",   -10  ),  # standard negative case
@@ -59,43 +129,22 @@ class TestHW2( unittest.TestCase ):
 				( (  -11,   10, None ), "-10",   -10  ),  # input greater than lbound & less than ubound
 				( (  -11,   10, None ), "0",       0  ))  # zero input greater than lbound & less than ubound
 
+		self.callSolicitInteger( valid, None )
 
-		use_cases = (
-				( invalid_garbage, None ),
-				( invalid_toolow,  "too hi" ),
-				( invalid_toohigh, "too lo" ),
-				( valid_default, None ),
-				( valid, None ))
-
-		for inputs, expected_message in use_cases:
-			for _args, _input, _output in inputs:
-				self.student_module.raw_input = lambda _: _input
-				retval = None
-				try:
-					retval = self.student_module.SolicitInteger( *args )
-					self.assertEqual( retval, _output )
-					output = sys.stdout.getvalue().strip()
-					if expected_message:
-						self.assertIn( output.lower(), expected_message )
-
-				except Exception as e:
-					self.testFail( "Uncaught exception in SolicitInteger when calling with " + \
-							"lbound={}, hibound={}, default_return={}, input={}".format( *_args, _input ))
-
-		self.student_module.raw_input = raw_input
-
-
+	#============================================
+	@unittest.expectedFailure
 	def test_RunTurn( self ):
-		self.assertTrue( True )
 		# enter trash and see if it's dealt with
 		# Check to see that 0 is a valid lower and upper bound
 		# check that the number chosen by the computer is really random by running RunTurn 5 times
-		RunTurn(-1000,1000)
+
+
+		stu_mod.RunTurn(-1000,1000)
 
 if __name__ == '__main__':
 
-	#http://docs.python.org/2/library/unittest.html#unittest.TestResult.buffer
 
-	assert not hasattr(sys.stdout, "getvalue")
+	_ = TestModule()
 
-	unittest.main(module=__name__, buffer=True)
+	del sys.argv[1:]
+	unittest.main(buffer=True,verbosity=3)
