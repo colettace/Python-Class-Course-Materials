279d86ecb0e3e707964976df39afc09dff1a8f5e
Chris Coletta <christopher.coletta@nih.gov>
2014-04-07 04:22:33 -0400

    Changing all tabs in hw4 to 4 spaces

--- homework/hw4/biof309_hw4_template.py
+++ homework/hw4/biof309_hw4_template.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 FAES BIOF309 Introduction To Python
 Template File for Homework 4:
@@ -39,87 +38,87 @@ import re
 
 #==================================================================
 class NucleotideSequence( object ):
-	"""Abstract base class to be inherited by subclasses DNASequence and RNASequence.
+    """Abstract base class to be inherited by subclasses DNASequence and RNASequence.
 
-	Your assignment is to replace each "raise NotImplementedError" line with your
-	implementation of that method.
-	
-	This class is not meant to be used in the wild, meaning you wouldn't ever 
-	instantiate an object of class NucleotideSequence, but rather you would only ever
-	instantiate an object of DNASequence or RNASequence, and that the functionality
-	you're about to implement here in the base class would be "inherited" by the subclasses.
-	"""
+    Your assignment is to replace each "raise NotImplementedError" line with your
+    implementation of that method.
+    
+    This class is not meant to be used in the wild, meaning you wouldn't ever 
+    instantiate an object of class NucleotideSequence, but rather you would only ever
+    instantiate an object of DNASequence or RNASequence, and that the functionality
+    you're about to implement here in the base class would be "inherited" by the subclasses.
+    """
 
-	def __init__( self, sequence=None ):
-		self._data = sequence
+    def __init__( self, sequence=None ):
+        self._data = sequence
 
-	# YOU MUST IMPLEMENT THE FOLLOWING METHODS FOR A MAXIMUM GRADE OF C:
+    # YOU MUST IMPLEMENT THE FOLLOWING METHODS FOR A MAXIMUM GRADE OF C:
 
-	def __len__( self ):
-		"""This is the method Python will run when the user calls len() on this instance.
-		Return the total number of ucleotides in the sequence contained in self._data."""
-		raise NotImplementedError
+    def __len__( self ):
+        """This is the method Python will run when the user calls len() on this instance.
+        Return the total number of ucleotides in the sequence contained in self._data."""
+        raise NotImplementedError
 
-	def __str__( self ):
-		"""This is the method Python will run when the user calls str() on this instance"""
-		raise NotImplementedError
+    def __str__( self ):
+        """This is the method Python will run when the user calls str() on this instance"""
+        raise NotImplementedError
 
-	def __repr__( self ):
-		"""This is the method Python will run when the user types the name of the
-		instance in a Python interpreter."""
-		raise NotImplementedError
+    def __repr__( self ):
+        """This is the method Python will run when the user types the name of the
+        instance in a Python interpreter."""
+        raise NotImplementedError
 
-	def __contains__( self, substr ):
-		"""This is the method Python will run when the user wants to use the keyword "in",
-		e.g., "TAG" in my_sequence."""
-		raise NotImplementedError
+    def __contains__( self, substr ):
+        """This is the method Python will run when the user wants to use the keyword "in",
+        e.g., "TAG" in my_sequence."""
+        raise NotImplementedError
 
-	def count( self, substring, start=0, end=sys.maxint ):
-		"""Return the number of times the substring appears in the sequence.
-		This function should work just like str.count."""
-		raise NotImplementedError
+    def count( self, substring, start=0, end=sys.maxint ):
+        """Return the number of times the substring appears in the sequence.
+        This function should work just like str.count."""
+        raise NotImplementedError
 
-	def IsValidSequence( self ):
-		"""This function should raise a ValueError if it detects a character
-		which is not a valid nucleotide character for its molecule type."""
-		raise NotImplementedError
+    def IsValidSequence( self ):
+        """This function should raise a ValueError if it detects a character
+        which is not a valid nucleotide character for its molecule type."""
+        raise NotImplementedError
 
-	def GCContent( self ):
-		"""Return the fraction of nucleotides in this sequence that are Cytosine and Guanine"""
-		raise NotImplementedError
+    def GCContent( self ):
+        """Return the fraction of nucleotides in this sequence that are Cytosine and Guanine"""
+        raise NotImplementedError
 
 
-	#==================================================================
-	# YOU MUST IMPLEMENT THE FOLLOWING METHODS FOR A MAXIMUM GRADE OF B
+    #==================================================================
+    # YOU MUST IMPLEMENT THE FOLLOWING METHODS FOR A MAXIMUM GRADE OF B
 
-	@classmethod
-	def NewFromFastaFile( cls, path_to_fasta_file ):
-		"""This function should read a nucleotide sequence from a fasta file,
-		parse it, check if the nucleotide sequence is valid by calling IsValidSequence(),
-		instantiate a new object, populate its instance attribute "_data" with the sequence,
-		and finally return the new instance."""
-		raise NotImplementedError
+    @classmethod
+    def NewFromFastaFile( cls, path_to_fasta_file ):
+        """This function should read a nucleotide sequence from a fasta file,
+        parse it, check if the nucleotide sequence is valid by calling IsValidSequence(),
+        instantiate a new object, populate its instance attribute "_data" with the sequence,
+        and finally return the new instance."""
+        raise NotImplementedError
 
-	#==================================================================
-	def TranslateToAA( self, directionality5to3=True, reading_frame=1 ):
-		"""Return the sequence translated into amino acids. The user is able to specify
-		the direction of translation (5'->3' or vice versa) and the reading frame offset,
-		either 1, 2, or 3"""
-		raise NotImplementedError
+    #==================================================================
+    def TranslateToAA( self, directionality5to3=True, reading_frame=1 ):
+        """Return the sequence translated into amino acids. The user is able to specify
+        the direction of translation (5'->3' or vice versa) and the reading frame offset,
+        either 1, 2, or 3"""
+        raise NotImplementedError
 
-	#==================================================================
-	# YOU MUST IMPLEMENT THE FOLLOWING METHOD FOR A MAXIMUM GRADE OF A
+    #==================================================================
+    # YOU MUST IMPLEMENT THE FOLLOWING METHOD FOR A MAXIMUM GRADE OF A
 
-	def OpenReadingFrames( self, directionality5to3=True, reading_frame=1 ):
-		"""This function should return a list of tuples which contain, for the given 
-		directionality and reading frame, 1. the aa start position, 2. aa end position, and 
-		3. amino acid sequence of every open reading frame in the sequence. Remember,
-		ORFs start with a Methionine (generally) and end with a stop codon. Hint:
-		use a regular expression."""
-		raise NotImplementedError
+    def OpenReadingFrames( self, directionality5to3=True, reading_frame=1 ):
+        """This function should return a list of tuples which contain, for the given 
+        directionality and reading frame, 1. the aa start position, 2. aa end position, and 
+        3. amino acid sequence of every open reading frame in the sequence. Remember,
+        ORFs start with a Methionine (generally) and end with a stop codon. Hint:
+        use a regular expression."""
+        raise NotImplementedError
 
-	# end class NucleotideSequence definition
-	#==================================================================
+    # end class NucleotideSequence definition
+    #==================================================================
 
 dna_codon_dict = { "TTT" : "F", "TCT" : "S", "TAT" : "Y", "TGT" : "C", \
 "TTC" : "F", "TCC" : "S", "TAC" : "Y", "TGC" : "C", \
@@ -157,26 +156,26 @@ rna_codon_dict = { "UUU" : "F", "UCU" : "S", "UAU" : "Y", "UGU" : "C", \
 
 class DNASequence( NucleotideSequence ):
 
-	# Class attributes
-	_translation_table = dna_codon_dict
-	_valid_nts = "AGCT"
+    # Class attributes
+    _translation_table = dna_codon_dict
+    _valid_nts = "AGCT"
 
-	# All other attributes inherited from base class
+    # All other attributes inherited from base class
 
 class RNASequence( NucleotideSequence ):
 
-	# Class Attributes
-	_translation_table = rna_codon_dict
-	_valid_nts = "AGCU"
+    # Class Attributes
+    _translation_table = rna_codon_dict
+    _valid_nts = "AGCU"
 
-	# All other attributes inherited from base class
+    # All other attributes inherited from base class
 
 if __name__ == '__main__':
 
-	DNAseq = DNASequence.NewFromFastaFile( 'zscan4.fasta' )
+    DNAseq = DNASequence.NewFromFastaFile( 'zscan4.fasta' )
 
-	reading_frames = ( (True, 1), (True, 2), (True, 3), (False, 1), (False, 2), (False, 3) )
+    reading_frames = ( (True, 1), (True, 2), (True, 3), (False, 1), (False, 2), (False, 3) )
 
-	for direction, reading_frame in reading_frames:
-		print "{} direction, reading frame {}:".format( "5'to3'" if direction else "3'to5'", reading_frame )
-		print DNAseq.OpenReadingFrames( direction, reading_frame )
+    for direction, reading_frame in reading_frames:
+        print "{} direction, reading frame {}:".format( "5'to3'" if direction else "3'to5'", reading_frame )
+        print DNAseq.OpenReadingFrames( direction, reading_frame )
